3e59e3aef2677d1a556a11bb4ba6b55de42f9624
aditya <111045036+aditya4737@users.noreply.github.com>
2023-10-30 23:03:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var randomnumber1 = Math.floor(Math.random()*100)+1;
 
 if(randomnumber1%2===0)
 {
-    document.querySelector("h1").innerHTML="Heads";
-    var imagesrc2 = "images\heads" + randomnumber1 + ".png";
+    document.querySelector("h1").innerHTML="Tails";
+    var imagesrc2 = "images/heads" + randomnumber1 + ".png";
     document.querySelectorAll("img")[0].setAttribute("src", imagesrc2);
-    console.log(imagesrc2+"  "+ h);
+    console.log(imagesrc2);
 }
 else
 {
-    document.querySelector("h1").innerHTML="Tails";
-    var imagesrc2 = "images\tails" + randomnumber1 + ".png";
+    document.querySelector("h1").innerHTML="Heads";
+    var imagesrc2 = "images/tails" + randomnumber1 + ".png";
     document.querySelectorAll("img")[1].setAttribute("src", imagesrc2);
-    console.log(imagesrc2+"  "+t);
+    console.log(imagesrc2);
 }
